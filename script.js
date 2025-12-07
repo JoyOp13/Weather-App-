4cf9adb4331acb375ea4cb15e133ce7295b242d7
Jay Patil <patiljay7545@gmail.com>
2025-12-07 17:16:37 +0530

    Adding Initial Files

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.getElementById("city-input");
+const button = document.getElementById("search-btn");
+
+const cityName = document.getElementById("city-name");
+const cityTime = document.getElementById("city-time");
+const cityTemp = document.getElementById("city-temp");
+
+async function getData(cityname1){
+    const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=dc86f2b39a004c1eb0463151250212&q=${cityname1}&aqi=yes
+    `);
+    return await data.json(); 
+}
+
+button.addEventListener("click", async ()=>{
+    const value = input.value;
+    const result = await getData(value);
+    // console.log(result)
+    cityName.innerText =`${result.location.name}, ${result.location.region} - ${result.location.name}`
+    cityTime.innerText = result.location.localtime ;
+    cityTemp.innerText = result.current.temp_c;
+})
